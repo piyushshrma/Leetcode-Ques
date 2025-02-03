b83b255adcbdc2beae304d7c253a2e3c82ac2c20
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-02-03 20:08:24 +0530

    Time: 4 ms (15.24%), Space: 10.4 MB (11.22%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removing(string str, string part, int len) {
+        int st = str.find(part);
+            str.erase(st, len);  // Erase the found part
+        return str;
+    }
+
+    string removeOccurrences(string str, string part) {
+        int len = part.length();
+        while (str.find(part) < str.length()) {  // Keep removing part while it's found
+            str = removing(str, part, len);  // Reassign the result back to str
+        }
+        return str;
+    }
+};
