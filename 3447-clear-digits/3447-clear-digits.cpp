9f30b7438a6b914c3b748bd82384b741f5af3ca9
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-02-10 09:11:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (27.61%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            if((s[i]>='a' && s[i]<='z')){
+                st.push(s[i]);
+            }
+            else if(s[i]>='0' && s[i]<='9'){
+                st.pop();
+            }
+        }
+        string ns;
+        while (!st.empty()) {
+        char top = st.top();  // Get the top element
+        ns+=top;     // Print the top element
+        st.pop();             // Pop the top element
+    }
+        reverse(ns.begin(), ns.end());
+
+        return ns;
+    }
+};
