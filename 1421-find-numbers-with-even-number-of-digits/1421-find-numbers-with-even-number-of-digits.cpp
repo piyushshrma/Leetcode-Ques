5c562d8702ebe33d8a72db2088614c8300f44191
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-02-10 19:53:40 +0530

    Time: 1 ms (14.77%), Space: 14.5 MB (12.25%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -13,3 +13,31 @@ public:
         return count;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool even(int num){
+//         int digitCount = 0;
+//         if (num == 0) {
+//             digitCount = 1;  // special case for 0 as it has 1 digit
+//         } else {
+//             while (num != 0) {
+//                 num /= 10;
+//                 digitCount++;
+//             }
+//         }
+//         return digitCount % 2 == 0;
+//     }
+
+//     int findNumbers(vector<int>& nums) {
+//         int count = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (even(nums[i])) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
