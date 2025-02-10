f7c6a0a7f4ed097dad9d53920ce91303a608dd8e
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-02-10 19:07:00 +0530

    Time: 3 ms (11.76%), Space: 14.1 MB (47.3%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -14,3 +14,20 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> finalPrices(vector<int>& prices) {
+//         int n=prices.size();
+//         vector<int> ans=prices;
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 if(j>i && prices[j]<=prices[i]){
+//                     ans[i]=prices[i]-prices[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
