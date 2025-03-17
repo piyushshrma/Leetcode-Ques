c925a886e4a35da486dc6c82c0742cee5cdba7c3
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-03-17 10:37:36 +0530

    Time: 0 ms (100%), Space: 16.7 MB (84.67%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -3,12 +3,13 @@ public:
     bool divideArray(vector<int>& nums) {
         int count=0;
         int n=nums.size();
+        int pairs=n/2;
         sort(nums.begin(),nums.end());
         for(int i=1; i<nums.size(); i+=2){
                 if(nums[i]==nums[i-1]){
                     count++;
             }
         }
-        return (count>=n/2);
+        return (count>=pairs);
     }
 };
