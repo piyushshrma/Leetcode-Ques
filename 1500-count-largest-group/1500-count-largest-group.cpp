702be99c11b31507804fa9e67af0846fb5092f30
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-04-25 09:00:51 +0530

    Update 1500-count-largest-group.cpp

--- 1500-count-largest-group/1500-count-largest-group.cpp
+++ 1500-count-largest-group/1500-count-largest-group.cpp
@@ -25,3 +25,46 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+
+//     int digitSum(int num){
+//         int sum=0;
+//         while(num){
+//             int rem=num%10;
+//             sum+=rem;
+//             num=num/10;
+//         }
+//         return sum;
+//     }
+
+//     int countLargestGroup(int n) {
+//         if(n<=9){
+//             return n;
+//         }
+//         map<int, int> mp;
+//         for(int i=1; i<=n; i++){
+//             if(i<=9){
+//             mp[i]++;}
+
+//             else{
+//                 int thatSum = digitSum(i);
+//                 mp[thatSum]++;
+//             }
+//         }
+//         int maxAns=INT_MIN;
+//         for(auto& i : mp){
+//             maxAns=max(maxAns, i.second);
+//         }
+
+//         int ans=0;
+//         for(auto& i : mp){
+//             if(i.second==maxAns){
+//                 ans++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
