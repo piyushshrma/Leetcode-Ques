ccad5b92671335e4b14e9fd3195e2acfb2f9ad48
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-06-15 13:28:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (69.19%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     int maxDiff(int num) {
-        string str1 = to_string(num);
-        string str2 = str1;
-        int n = str1.size();
-        // Get max number
-        int i;
-        for(i = 0; i < n; i++) {
-            if(str1[i] != '9') break;
-        }
-        char ele1 = str1[i];
-        for(int k = 0; k < n; k++) {
-            if(str1[k] == ele1) str1[k] = '9';
-        }
-        // Get min number
-        char ele2 = str2[0];
-        char replace = '1';
-        if(ele2 == '1') {
-            for(int k = 1; k < n; k++) {
-                if(str2[k] != '0' && str2[k] != '1') {
-                    ele2 = str2[k];
-                    replace = '0';
-                    break;
-                }
-            }
-        }
-        for(int k = 0; k < n; k++) {
-            if(str2[k] == ele2) str2[k] = replace;
+        string str1=to_string(num);
+        string str2=to_string(num);
+
+        int idx=str1.find_first_not_of('9');
+        if(idx!=string::npos){
+            char ch=str1[idx];
+            replace(str1.begin(), str1.end(), ch, '9');
         }
 
+        for(int i=0; i<str2.size(); i++){
+            char ch=str2[i];
+
+            if(i==0){
+                if(ch!='1'){
+                    replace(str2.begin(), str2.end(), ch, '1');
+                    break;
+                }
+            }   else if(ch!='0' && str2[i]!=str2[0]){
+                    replace(str2.begin(), str2.end(), ch, '0');
+                    break;
+                }
+        }
         return stoi(str1) - stoi(str2);
     }
 };
