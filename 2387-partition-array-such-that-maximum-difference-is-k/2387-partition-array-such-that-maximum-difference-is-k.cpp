a01b535462fe8adaea891416e074e4baf915b527
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-06-19 09:13:18 +0530

    Time: 116 ms (5.15%), Space: 86.9 MB (39.38%) - LeetHub

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        //Sort the array nums.
+        sort(nums.begin(), nums.end());
+        int n=nums.size();
+        int ct=0, i=0;
+
+        while(i<n){
+            int st=nums[i];
+            int j=i;
+
+        // Keep extending the subsequence while the condition is met
+        while(j<n && nums[j] - st <= k){
+            j++;
+        }
+        // one subsequence formed
+        ct++;
+
+        i=j;
+        }
+
+        return ct;
+    }
+};
