cbacb029ce3cf0145357edfd887914fc4f221021
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-07-01 18:23:23 +0530

    Time: 47 ms (61.03%), Space: 295.4 MB (19.63%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -11,35 +11,62 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        vector<int> ll;
-        ListNode* temp = head;
+        ListNode* temp=head;
+        ListNode* heady = new ListNode(0);
+        ListNode* curr = heady;
 
-        //vector me saari val daal di
-        while(temp!=NULL){
-            ll.push_back(temp->val);
-            temp=temp->next;
-        }
-        
-        vector<int> nayavec;
         int sum=0;
-        for(int i=0; i<ll.size(); i++){
-            if(ll[i]!=0){
-                sum+=ll[i];
+
+        while(temp!=NULL){
+            if(temp->val != 0){
+                sum+=temp->val;
             }
             else{
                 if(sum!=0){
-                nayavec.push_back(sum);
-                sum=0;
+                    curr->next = new ListNode(sum);
+                    curr=curr->next;
+                    sum=0;
                 }
             }
+            temp=temp->next;
         }
-
-        ListNode* heady = new ListNode(nayavec[0]);
-        ListNode* tempy = heady;
-        for(int i=1; i<nayavec.size(); i++){
-            tempy->next=new ListNode(nayavec[i]);
-            tempy=tempy->next;
-        }
+        heady=heady->next;
         return heady;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* mergeNodes(ListNode* head) {
+//         vector<int> ll;
+//         ListNode* temp = head;
+
+//         //vector me saari val daal di
+//         while(temp!=NULL){
+//             ll.push_back(temp->val);
+//             temp=temp->next;
+//         }
+        
+//         vector<int> nayavec;
+//         int sum=0;
+//         for(int i=0; i<ll.size(); i++){
+//             if(ll[i]!=0){
+//                 sum+=ll[i];
+//             }
+//             else{
+//                 if(sum!=0){
+//                 nayavec.push_back(sum);
+//                 sum=0;
+//                 }
+//             }
+//         }
+
+//         ListNode* heady = new ListNode(nayavec[0]);
+//         ListNode* tempy = heady;
+//         for(int i=1; i<nayavec.size(); i++){
+//             tempy->next=new ListNode(nayavec[i]);
+//             tempy=tempy->next;
+//         }
+//         return heady;
+//     }
+// };
