9ba4085def07f0a29aa89e664713111a49d7f0d2
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-07-26 11:36:50 +0530

    Time: 3 ms (19.87%), Space: 71.7 MB (80.78%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -15,3 +15,19 @@ public:
         return maxSum;
     }
 };
+
+// Brute Force
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int maxSum=INT_MIN;
+//         for(int i=0; i<nums.size(); i++){
+//             int currSum=0;
+//             for(int j=i; j<nums.size(); j++){
+//                 currSum+=nums[j];
+//                 maxSum=max(currSum, maxSum);
+//             }
+//         }
+//         return maxSum;
+//     }
+// };
