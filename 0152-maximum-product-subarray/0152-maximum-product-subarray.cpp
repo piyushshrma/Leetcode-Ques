4d703ffa68038b112f426e0af784e38a94e7dd38
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-07-26 23:50:10 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.59%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -11,15 +11,17 @@ public:
                 currMul=1;
             }
         }
-        
+
         currMul = 1;
 
-        // Right to left
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            currMul *= nums[i];
-            maxMul = max(maxMul, currMul);
-            if (currMul == 0) currMul = 1;
+        for(int i=nums.size()-1; i>=0; i--){
+            currMul*=nums[i];
+            maxMul=max(currMul, maxMul);
+            if(currMul==0){
+                currMul=1;
+            }
         }
+        
 
         return maxMul;
     }
