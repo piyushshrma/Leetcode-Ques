a6c431d4ad686bae0ece75a857d0227b774a2687
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-07-30 19:54:34 +0530

    Time: 11 ms (6.73%), Space: 12.2 MB (91.83%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,55 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans(nums2.size());
-        stack<int> s;
-        for(int i=nums2.size()-1; i>=0; i--){
-            while(s.size()>0 && s.top()<=nums2[i]){
-                s.pop();
-            }
-            if(s.empty()) ans[i]=-1;
-            else ans[i]=s.top();
-            s.push(nums2[i]);
-        }
-        vector<int> finans(nums1.size());
+        vector<int> ans(nums1.size(), -1);
+
         for(int i=0; i<nums1.size(); i++){
+            bool found=false;
             for(int j=0; j<nums2.size(); j++){
                 if(nums1[i]==nums2[j]){
-                    finans[i]=ans[j];
+                    found=true;
+                }
+                if(found && nums2[j]>nums1[i]){
+                    ans[i]=nums2[j];
+                    break;
                 }
             }
         }
-        return finans;
+        return ans;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-//         unordered_map<int, int> nextGreaterMap;
-//         stack<int> s;
-        
-//         // Fill the map with next greater elements for nums2
-//         for (int i = nums2.size() - 1; i >= 0; i--) {
-//             while (!s.empty() && s.top() <= nums2[i]) {
-//                 s.pop();
-//             }
-//             if (s.empty()) {
-//                 nextGreaterMap[nums2[i]] = -1;
-//             } else {
-//                 nextGreaterMap[nums2[i]] = s.top();
-//             }
-//             s.push(nums2[i]);
-//         }
-        
-//         // Now, for each element in nums1, find the next greater element using the map
-//         vector<int> result;
-//         for (int num : nums1) {
-//             result.push_back(nextGreaterMap[num]);
-//         }
-        
-//         return result;
-//     }
-// };
