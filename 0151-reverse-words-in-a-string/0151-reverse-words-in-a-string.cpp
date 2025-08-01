edd620bde259bee5a8e7aade41f0c856b6920a14
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-08-01 14:33:13 +0530

    Time: 3 ms (42.22%), Space: 10.1 MB (85.03%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
         string ans="";
         string temp="";
 
-        for(int i=n-1; i>=0; i--){
+        for(int i=s.size()-1; i>=0; i--){
             if(s[i]!=' '){
                 temp+=s[i];
             }
@@ -18,13 +17,16 @@ public:
         }
 
         if(!temp.empty()){
-            reverse(temp.begin(), temp.end());
-            ans+=temp;
-            temp="";
+                reverse(temp.begin(), temp.end());
+                ans+=temp;
+                ans+=" ";
+                temp="";
         }
+
         if(ans[ans.size()-1]==' '){
             ans.pop_back();
         }
+
         return ans;
     }
 };
