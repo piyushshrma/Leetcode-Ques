dd56d0b2526e15b0d306e112fb8ded79c1981c37
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-08-03 15:37:53 +0530

    Time: 0 ms (100%), Space: 19.4 MB (62.74%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,32 +10,54 @@
  */
 class Solution {
 public:
+    ListNode* merge(ListNode* h1, ListNode* h2){
+        if(!h1) return h2;
+        if(!h2) return h1;
+
+        if(h1->val <= h2->val){
+            h1->next = merge(h1->next, h2);
+            return h1;
+        }
+        else{
+            h2->next=merge(h1, h2->next);
+            return h2;
+        }
+    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> nums;
-        ListNode* temp1=list1;
-        ListNode* temp2=list2;
-
-        if (list1 == NULL && list2 == NULL) return NULL;
-
-        while(temp1!=NULL){
-            nums.push_back(temp1->val);
-            temp1=temp1->next;
-        }
-
-        while(temp2!=NULL){
-            nums.push_back(temp2->val);
-            temp2=temp2->next;
-        }
-
-        sort(nums.begin(), nums.end());
-
-        ListNode* head = new ListNode(nums[0]);
-        ListNode* current = head;
-
-        for (int i = 1; i < nums.size(); ++i) {
-        current->next = new ListNode(nums[i]);
-        current = current->next;
-        }
-        return head;
+        return merge(list1, list2);
     }
 };
+
+
+// using extra space of vector
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         vector<int> nums;
+//         ListNode* temp1=list1;
+//         ListNode* temp2=list2;
+
+//         if (list1 == NULL && list2 == NULL) return NULL;
+
+//         while(temp1!=NULL){
+//             nums.push_back(temp1->val);
+//             temp1=temp1->next;
+//         }
+
+//         while(temp2!=NULL){
+//             nums.push_back(temp2->val);
+//             temp2=temp2->next;
+//         }
+
+//         sort(nums.begin(), nums.end());
+
+//         ListNode* head = new ListNode(nums[0]);
+//         ListNode* current = head;
+
+//         for (int i = 1; i < nums.size(); ++i) {
+//         current->next = new ListNode(nums[i]);
+//         current = current->next;
+//         }
+//         return head;
+//     }
+// };
