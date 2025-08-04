f91997fab50b8380075595204f9c58a1685d0132
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-08-04 20:14:58 +0530

    Time: 44 ms (10.93%), Space: 68.6 MB (7.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxLen=0;
-        int n=s.length();
-        if(n<=0){
-            return 0;
-        }
+        int maxC=0;
+        int n=s.size();
         for(int i=0; i<n; i++){
             vector<int> freq(128,0);
             int curr=0;
             for(int j=i; j<n; j++){
-                
                 if(freq[s[j]]==0){
                     curr++;
                     freq[s[j]]++;
-                    maxLen=max(maxLen, curr);
+                    maxC=max(maxC, curr);
                 }
                 else{
                     break;
                 }
             }
         }
-        return maxLen;
+        return maxC;
     }
 };
