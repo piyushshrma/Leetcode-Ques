8be18a53530dd1419fc731dce0eb19cacd9542b3
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-08-19 17:23:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,30 @@
-// Dutch National Flag Algo
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int z=0,o=0,t=0;
         int n=nums.size();
-        int low=0,mid=0,high=n-1;
-
-        while(mid<=high){
-        if(nums[mid]==0){
-        swap(nums[low],nums[mid]);
-        mid++;
-        low++;}
-        else if(nums[mid]==1){
-        mid++;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                z++;
+            }
+            if(nums[i]==1){
+                o++;
+            }
+            if(nums[i]==2){
+                t++;
+            }
         }
-        else{
-        swap(nums[high],nums[mid]);
-        high--;
-        }
-    }
+        if(z>=1){
+        for(int i=0; i<o; i++){
+            nums[i]=0;
+        }}
+        if(o>=1){
+        for(int i=z; i<o+z; i++){
+            nums[i]=1;
+        }}
+        if(t>=1){
+        for(int i=o+z; i<o+z+t; i++){
+            nums[i]=2;
+        }}
     }
 };
-
-
-// class Solution {
-// public:
-//     void sortColors(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//     }
-// };
