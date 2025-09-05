b2d8c883593f35cc91bb4fd842ef5116040fe5f2
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-09-05 11:55:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    vector<int> dp = vector<int>(46, -1);
-
-    int climbStairs(int n) {
+    int rec(int n, vector<int>& dp){
+        //Base Case
         if(n==0 || n==1){
             return 1;
         }
+
+        //Check
         if(dp[n]!=-1){
             return dp[n];
         }
-        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+
+        return dp[n]=rec(n-1,dp)+rec(n-2,dp);        
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return rec(n,dp);
     }
 };
-
-// class Solution {
-// public:
-//     const int MOD = 1000000007;
-
-//     int climbStairs(int n) {
-//         if(n==0 || n==1){
-//             return 1;
-//         }
-
-//         vector<long long> dp(n+1,-1);
-
-//         dp[0] = 1;
-//         dp[1] = 1;
-
-//         for (int i = 2; i <= n; i++) {
-//         dp[i] = (dp[i - 1] + dp[i - 2]);
-//         }
-//         return dp[n];
-//     }
-// };
