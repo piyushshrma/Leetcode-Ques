f43ceed46cba5f809c369ab8923197ca97b37d81
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-09-07 21:54:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int rec(int i, vector<int>& nums, vector<int>& dp){
+    int rec(int i, int n, vector<int>& nums, vector<int>& dp){
         //base case
-        if(i>=nums.size()){
+        if(i>=n){
             return 0;
         }
 
         //check
-        if(dp[i]!=-1) return dp[i];
+        if(dp[i]!=-1){
+            return dp[i];
+        }
 
-        int take=nums[i]+rec(i+2,nums,dp);
+        //take
+        int take=nums[i]+rec(i+2,n,nums,dp);
 
-        int not_take=rec(i+1,nums,dp);
+        //not take
+        int not_take=rec(i+1,n,nums,dp);
 
-        return dp[i] = max(take, not_take);
+        return max(take, not_take);
     }
-
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1, -1);
-        return rec(0, nums,dp);
+        int n=nums.size();
+        vector<int> dp(n+1, -1);
+
+        return rec(0,n,nums,dp);
     }
 };
