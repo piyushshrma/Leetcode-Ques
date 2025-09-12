36b1e4a75f796667c117a359d331cc4cecc20937
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2025-09-12 17:52:04 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,18 +5,20 @@ public:
             return 0;
         }
 
+        //check
         if(dp[i]!=-1) return dp[i];
 
-        int take=nums[i] + rec(i+2, n, nums, dp);
+        //take
+        int take=nums[i]+rec(i+2,n,nums,dp);
 
-        int not_take=rec(i+1, n, nums, dp);
+        //nottake
+        int nottake=rec(i+1,n,nums,dp);
 
-        return dp[i] = max(take, not_take);
+        return dp[i]=max(take, nottake);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n+1, -1);
-
         return rec(0,n,nums,dp);
     }
 };
